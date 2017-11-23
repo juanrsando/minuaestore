e166f697346ef4a406ab09c337bd2c55958c2d08
juanrsando <jrsando_96@hotmail.com>
2017-11-22 21:48:06 -0600

    agredo gem carrierwave y lo necesario en la form de product para subir foto al crear producto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -77,6 +77,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :category_id, :size, :color, :price)
+      params.require(:product).permit(:name, :description, :category_id, :size, :color, :price, :stock, :photo, :photo_cache)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122194227) do
+ActiveRecord::Schema.define(version: 20171123032914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20171122194227) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "stock", default: 0
+    t.string "photo"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
