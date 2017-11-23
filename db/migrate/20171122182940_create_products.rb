fed7eddd238650b8716dd398e494c98530ccfb53
juanrsando <jrsando_96@hotmail.com>
2017-11-22 22:21:40 -0600

    ya jala carrierwave

--- db/migrate/20171122182940_create_products.rb
+++ db/migrate/20171122182940_create_products.rb
@@ -1,12 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
-      t.string :name
-      t.text :description
+      t.string :name, default:"NOT GIVEN"
+      t.text :description, default:"NOT GIVEN"
       t.references :category, foreign_key: true
-      t.string :size
-      t.string :color
-      t.float :price
+      t.string :size, default:"NOT GIVEN"
+      t.string :color, default:"NOT GIVEN"
+      t.float :price, default:0
 
       t.timestamps
     end
--- db/migrate/20171123032914_add_photo_to_product.rb
+++ db/migrate/20171123032914_add_photo_to_product.rb
@@ -1,5 +1,5 @@
 class AddPhotoToProduct < ActiveRecord::Migration[5.1]
   def change
-    add_column :products, :photo, :string
+    add_column :products, :photo, :string, default_url:'http://blog.desafiolatam.com/wp-content/uploads/2015/03/logo_latam_mini.png'
   end
 end
